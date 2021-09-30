15171818db6cf69465e581a6a4d93b5851acc176
atisheks1 <84221898+atisheksingh@users.noreply.github.com>
2021-09-30 13:35:12 +0530

    Update interaction with uniswapv3.js

--- interaction with uniswapv3.js
+++ interaction with uniswapv3.js
@@ -1,6 +1,6 @@
 
 
-const url = 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
+const url = 'https://rinkeby.infura.io/v3/<--enter your endpoint here-->';
 
 const Web3 = require('web3');
 var web3 = new Web3(url);
